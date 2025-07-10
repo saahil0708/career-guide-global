48f6da13000198921b558c2ea5b2d7eeb0e65f34
Saahil <saahil.ind.dgp@gmail.com>
2025-07-10 11:26:41 +0530

    Logo Updated

--- src/app/Components/Global/Footer.jsx
+++ src/app/Components/Global/Footer.jsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import Image from "next/image"
-import Logo from '../../../Images/CCGL.png'
+import Logo from '../../../Images/CCGLW.png'
 import {
   MapPin,
   Phone,
