0c530d7cb4e602b1742933ef639c67869decff15
Saahil <saahil.ind.dgp@gmail.com>
2026-06-07 16:16:45 +0530

    ..

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Maintenance from "./Components/Global/Maintenance";
 
 export const metadata = {
     title: 'Career Guide Global',
@@ -7,11 +8,16 @@ export const metadata = {
     }
 }
 
+// Set this to true to enable the maintenance / upgradation screen globally
+const MAINTENANCE_MODE = true;
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        {MAINTENANCE_MODE ? <Maintenance /> : children}
+      </body>
     </html>
   );
 }
+
